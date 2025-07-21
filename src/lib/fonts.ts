8fc54cb0a0c650a91e3e12dc75c972bf2a565565
Il Tuo Nome <tua.email@example.com>
2025-07-21 18:54:55 +0200

    feat: add Quadrat Serial font from src/lib/ and configure all weights

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -1,8 +1,33 @@
-import { Inter } from 'next/font/google'
+import localFont from 'next/font/local'
 
-export const trajan = Inter({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
+export const trajan = localFont({
+  src: [
+    {
+      path: './fonnts.com-quadrat_serial-light.otf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: './fonnts.com-quadrat_serial-regular.otf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: './fonnts.com-quadrat_serial-medium.otf',
+      weight: '500',
+      style: 'normal',
+    },
+    {
+      path: './fonnts.com-quadrat_serial-bold.otf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: './fonnts.com-quadrat_serial-xbold.otf',
+      weight: '800',
+      style: 'normal',
+    },
+  ],
   variable: '--font-trajan',
   display: 'swap',
 }) 
